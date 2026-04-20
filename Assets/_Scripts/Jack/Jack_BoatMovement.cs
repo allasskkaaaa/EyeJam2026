816c4264f1161601b0f30fd850e734feeb0e2fb9
JackMorgan101 <80333498+JackMorgan101@users.noreply.github.com>
2026-04-20 23:54:52 +0100

    Boat Movement
    Boat movement with gentle rocking on the boat model itself. Boat movement works by pushing with a rowing motion
    Needs sounds and more visual way to demonstrate pushing

---
+++ Assets/_Scripts/Jack/Jack_BoatMovement.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jack_BoatMovement : MonoBehaviour
+{
+    public float acceleration;
+    public float maxSpeed;
+    public float rotationSpeed;
+    public float drag;
+    public float brakeForce;
+    [SerializeField] private Transform boatObj;
+
+    private Vector2 moveDir;
+    private float velocity;
+    [SerializeField] private float velocityDeadzone;
+    private bool doBrake;
+
+    public float strokeSpeed;
+    public float strokeStrength;
+    private float strokeTimer;
+
+    private void Update()
+    {
+        moveDir.x = Input.GetAxis("Horizontal");
+        moveDir.y = Input.GetAxis("Vertical");
+        doBrake = Input.GetKey(KeyCode.X);
+
+        BoatRotation();
+        BoatMovement();
+    }
+
+    void BoatRotation()
+    {
+        float rotateSpeed = moveDir.x * rotationSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.up, rotateSpeed);
+    }
+
+    void BoatMovement()
+    {
+        // Start the timer for strokes and increment by the stroke speed (Default is 6)
+        strokeTimer += Time.deltaTime * strokeSpeed;
+
+        // Use sine wave and only get the down-strokes
+        float stroke = Mathf.Sin(strokeTimer);
+        float strokeForce = Mathf.Max(0f, stroke);
+
+        // Check if movement is happening period
+        velocity += acceleration * strokeForce * moveDir.y * strokeStrength * Time.deltaTime;
+        if (doBrake)
+        {
+            velocity -= velocity * brakeForce * Time.deltaTime;
+        }
+        // Reduce by constant velocity, including an ever-present reduction that helps with making it slow faster as velocity reaches < ~0.1f
+        velocity -= velocity * drag * Time.deltaTime;
+        velocity -= Mathf.Sign(velocity) * 0.2f * Time.deltaTime;
+        // If ship is nearly completely still, stop all movement (also checks if you're trying to move forward, so as to prevent accidental stopping constantly)
+        if (Mathf.Abs(velocity) < velocityDeadzone && moveDir.y == 0f)
+        {
+            velocity = 0f;
+        }
+        velocity = Mathf.Clamp(velocity, -maxSpeed, maxSpeed);
+        transform.position += transform.forward * velocity * Time.deltaTime;
+    }
+
+}
